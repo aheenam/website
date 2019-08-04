481f3cb6aff2a15ab781e304cf3ccd419bfb1f69
Rathes Sachchithananthan <rathes@aheenam.com>
2019-08-04 22:30:46 +0200

    💄 Add styles to link

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,9 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1 className="font-serif font-bold text-5xl leading-none">
       We craft web and mobile apps.{" "}
-      <a href="mailto:rathes@aheenam.com">Say hi!</a>
+      <a className="underline" href="mailto:rathes@aheenam.com">
+        Say hi!
+      </a>
     </h1>
   </Layout>
 )
