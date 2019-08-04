fb0334bdec701366d8125b285a1a9d2d12a557ff
Rathes Sachchithananthan <rathes@aheenam.com>
2019-08-04 22:31:44 +0200

    🎨 Add aria-label to link

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,11 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1 className="font-serif font-bold text-5xl leading-none">
       We craft web and mobile apps.{" "}
-      <a className="underline" href="mailto:rathes@aheenam.com">
+      <a
+        aria-label="Send an E-Mail to rathes@aheenam.com"
+        className="underline"
+        href="mailto:rathes@aheenam.com"
+      >
         Say hi!
       </a>
     </h1>
