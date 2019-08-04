a4fcd8d3305aa919ad24c17857925a467c9ae9bf
Rathes Sachchithananthan <rathes@aheenam.com>
2019-08-04 13:04:30 +0200

    ✨ Pass className prop to main element

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,11 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import cx from "classnames"
 
 import "./layout.css"
 import Logo from "../Logo"
 
-const Layout = ({ children }) => {
+const Layout = ({ children, className }) => {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       <header className="p-4 md:p-8 lg:pl-32 lg:pt-16">
@@ -13,7 +14,9 @@ const Layout = ({ children }) => {
           <Logo />
         </Link>
       </header>
-      <main className="flex-grow p-4 md:p-8 lg:pl-32">{children}</main>
+      <main className={cx("flex-grow p-4 md:p-8 lg:pl-32", className)}>
+        {children}
+      </main>
       <footer className="p-4 text-xs md:p-8 lg:pl-32 lg:pb-16">
         <Link className="inline-block mr-2 underline text-gray-600" to="/legal">
           Legal information
