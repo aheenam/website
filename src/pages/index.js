543e8c53055ccd5e888e14182c329973b9809b4b
Rathes Sachchithananthan <rathes@aheenam.com>
2019-08-04 13:04:42 +0200

    💄 Align title

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <Layout className="flex flex-col justify-center">
     <SEO title="Home" />
     <h1>We craft web and mobile apps. Say hi!</h1>
   </Layout>
