829172f139247f18a08c2f1b9dcd42516b0cd717
Rathes Sachchithananthan <rathes@aheenam.com>
2019-08-04 22:48:54 +0200

    ✨ Introduce legal page

---
+++ src/pages/legal.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+
+const LegalPage = () => {
+  return (
+    <Layout>
+      <h1>Legal</h1>
+    </Layout>
+  )
+}
+
+export default LegalPage
