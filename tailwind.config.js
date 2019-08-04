07403a4e1a57e5c2d5c72aea63cedc23fcdf8fc6
Rathes Sachchithananthan <rathes@aheenam.com>
2019-08-04 22:33:50 +0200

    🔧 Add custom max-width

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
         serif: ["IBM Plex Serif", ...defaultTheme.fontFamily.serif],
       },
+      maxWidth: {
+        "570px": "570px",
+      },
     },
   },
   variants: {},
