fa895fab94f9da732cd25323aa744163823408d2
Rathes Sachchithananthan <rathes@aheenam.com>
2019-08-04 22:30:20 +0200

    ✨ Link to email address

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,8 @@ const IndexPage = () => (
   <Layout className="flex flex-col justify-center">
     <SEO title="Home" />
     <h1 className="font-serif font-bold text-5xl leading-none">
-      We craft web and mobile apps. Say hi!
+      We craft web and mobile apps.{" "}
+      <a href="mailto:rathes@aheenam.com">Say hi!</a>
     </h1>
   </Layout>
 )
