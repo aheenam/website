2a875ed5a2b46c2522884b0b9c80c5c814d8c2d1
Rathes Sachchithananthan <rathes@aheenam.com>
2019-08-04 22:28:59 +0200

    💄 Add styles to title

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout className="flex flex-col justify-center">
     <SEO title="Home" />
-    <h1>We craft web and mobile apps. Say hi!</h1>
+    <h1 className="font-serif font-bold text-5xl leading-none">
+      We craft web and mobile apps. Say hi!
+    </h1>
   </Layout>
 )
 
