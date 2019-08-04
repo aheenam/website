ea3eb3d4d4c79881dd7eb60557422a7afd17fe44
Rathes Sachchithananthan <rathes@aheenam.com>
2019-08-04 22:25:42 +0200

    🔧 Extend fonts stacks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
+        serif: ["IBM Plex Serif", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
