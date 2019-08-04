0c10ea5481ab1e7546a687f3930f1db9e63070b5
Rathes Sachchithananthan <rathes@aheenam.com>
2019-08-04 12:26:19 +0200

    💄 Add logo in layout header

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -2,10 +2,14 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import "./layout.css"
+import Logo from "../Logo"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <header>
+        <Logo />
+      </header>
       <main>{children}</main>
     </>
   )
